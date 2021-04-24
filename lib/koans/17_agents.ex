67e0f567a14a89006f898cfb5ab6c6bcec7b5007
Brian Meeker <meeker.brian@gmail.com>
2021-04-23 23:23:03 -0400

    Completed 17_agents

--- lib/koans/17_agents.ex
+++ lib/koans/17_agents.ex
@@ -5,12 +5,15 @@ defmodule Agents do
 
   koan "Agents maintain state, so you can ask them about it" do
     {:ok, pid} = Agent.start_link(fn -> "Hi there" end)
-    assert Agent.get(pid, & &1) == ___
+    # & &1 == fn state -> state end (Programming Elixir calls this the "identity function" on page 257)
+    # Also, 1.10 added Function.identity/1
+    assert Agent.get(pid, & &1) == "Hi there"
+    assert Agent.get(pid, &Function.identity/1) == "Hi there"
   end
 
   koan "Agents may also be named so that you don't have to keep the pid around" do
     Agent.start_link(fn -> "Why hello" end, name: AgentSmith)
-    assert Agent.get(AgentSmith, & &1) == ___
+    assert Agent.get(AgentSmith, & &1) == "Why hello"
   end
 
   koan "Update to update the state" do
@@ -20,7 +23,7 @@ defmodule Agents do
       String.upcase(old)
     end)
 
-    assert Agent.get(__MODULE__, & &1) == ___
+    assert Agent.get(__MODULE__, & &1) == "HI THERE"
   end
 
   koan "Use get_and_update when you need to read and change a value in one go" do
@@ -31,8 +34,8 @@ defmodule Agents do
         {old, ["Bread" | old]}
       end)
 
-    assert old_list == ___
-    assert Agent.get(__MODULE__, & &1) == ___
+    assert old_list == ["Milk"]
+    assert Agent.get(__MODULE__, & &1) == ["Bread", "Milk"]
   end
 
   koan "Somebody has to switch off the light at the end of the day" do
@@ -40,6 +43,6 @@ defmodule Agents do
 
     Agent.stop(__MODULE__)
 
-    assert Process.alive?(pid) == ___
+    assert Process.alive?(pid) == false
   end
 end
